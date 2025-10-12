5a2d0e273b56e66b2e77daac3b0dd638e7b12f0a
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-12 00:55:03 -0600

    Metadata - Metatags

--- first-steps/app/contact/page.tsx
+++ first-steps/app/contact/page.tsx
@@ -1,3 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Contact page',
+    description: 'Esta es la página de contacto de la empresa',
+    keywords: [
+        'Contacto',
+        'Angel',
+        'Información',
+    ]
+};
+
 export default function ContactPage() {
     return (
         <>
--- first-steps/app/pricing/page.tsx
+++ first-steps/app/pricing/page.tsx
@@ -1,3 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Pricing page',
+    description: 'Esta es la página de precios de mi servicio',
+    keywords: [
+        'Contacto',
+        'Angel',
+        'Información',
+    ]
+};
+
 export default function Pricing() {
     return (
         <>
