1e03b2890dbf2969419ca3a9a5ea4ff4e22b9f1c
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-12 21:14:13 -0600

    Pensemos en hojas y pequeños componentes

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,28 +1,15 @@
-'use client';
-import { useState } from "react";
-
+import { CartCounter } from '../../shopping-cart/components/CartCounter';
 
 export const metadata = {
-    title: 'Counter page',
+    title: 'Shopping cart',
     description: 'Contador de productos',
 };
 
 export default function CounterPage() {
-
-    const [count, setCount] = useState(5)
-
     return (
         <div className="flex flex-col items-center justify-center w-full h-full">
             <span>Producto en el carrito</span>
-            <span className="text-9xl">{count}</span>
-            <div className="flex">
-                <button
-                    onClick={() => setCount(count + 1)}
-                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100] mr-2">+1</button>
-                <button
-                    onClick={() => setCount(count - 1)}
-                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100] mr-2">-1</button>
-            </div>
+            <CartCounter value={20} />
         </div>
     );
 }
