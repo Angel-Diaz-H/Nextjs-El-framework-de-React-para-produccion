8cce0cb7a4e0d91e1617858cb7ac8ad97438fcee
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-06-20 21:18:10 -0600

    Layouts y Layouts anidados

---
+++ 01-first-steps/app/(general)/layout.tsx
@@ -0,0 +1,12 @@
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <h1>Hello mundo</h1>
+      {children}
+    </div>
+  );
+}
