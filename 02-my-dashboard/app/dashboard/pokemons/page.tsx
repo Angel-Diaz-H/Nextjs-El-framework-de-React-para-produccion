b9785a839e46fb1b83b82922b015785052b35158
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-06-23 20:35:47 -0600

    Asignar tipo de datos y mostrar imágenes

--- 02-my-dashboard/app/dashboard/pokemons/page.tsx
+++ 02-my-dashboard/app/dashboard/pokemons/page.tsx
@@ -1,11 +1,38 @@
-const getPokEmons = async (limit = 20, offset = 0) => {
-  const data = await fetch(
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from "next/image";
+
+const getPokEmons = async (
+  limit = 20,
+  offset = 0,
+): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`,
   ).then((res) => res.json());
-  return data;
+
+  const pokemons = data.results.map((pokemon) => ({
+    id: pokemon.name.split("/").at(-2)!,
+    name: pokemon.name,
+    url: pokemon.url,
+  }));
+
+  return pokemons;
 };
 
 export default async function PokemonsPage() {
-  const pokemons = await getPokEmons();
-  return <div>{JSON.stringify(pokemons)}</div>;
+  const pokemons = await getPokEmons(151);
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-wrap items-center justify-center gap-10">
+        <Image
+          src={
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/10.svg"
+          }
+          alt={"pokemon"}
+          width={100}
+          height={100}
+        />
+      </div>
+    </div>
+  );
 }
