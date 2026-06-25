89692438edbe4b4d76222ecb923a830439ac1ac6
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-06-25 16:49:04 -0600

    Metadata dinámica

--- 02-my-dashboard/app/dashboard/pokemon/[id]/page.tsx
+++ 02-my-dashboard/app/dashboard/pokemon/[id]/page.tsx
@@ -1,15 +1,29 @@
 import { Pokemon } from "@/pokemons";
+import { Metadata } from "next";
 
 interface Props {
   params: Promise<{ id: string }>;
 }
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params;
+  const pokemon = await getPokemon(id);
+
+  return {
+    title: `#${pokemon.id} - ${pokemon.name}`,
+    description: `Información sobre el pokémon ${pokemon.name}`,
+  };
+}
+
 const getPokemon = async (id: string): Promise<Pokemon> => {
   const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
     cache: "force-cache", //TODO: Cambiar en futuro
+
+    // next: {
+    //   revalidate: 60 * 60 * 24 * 30 * 6, // 6 meses
+    // },
   }).then((res) => res.json());
 
-  console.log(`Se cargó: ${pokemon.name}`);
   return pokemon;
 };
 
@@ -20,7 +34,8 @@ export default async function PokemonPage({ params }: Props) {
   return (
     <div>
       <h1>Pokemon {id}</h1>
-      {JSON.stringify(pokemon)}
+      {/* {JSON.stringify(pok1emon)} */}
+      {pokemon.name}
     </div>
   );
 }
