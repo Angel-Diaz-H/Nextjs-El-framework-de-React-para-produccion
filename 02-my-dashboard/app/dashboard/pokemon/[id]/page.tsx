37b57783b3edfae60b8ea0d5d3f126412e2170f9
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-06-25 16:39:28 -0600

    Cargar información del Pokemón por ID

--- 02-my-dashboard/app/dashboard/pokemon/[id]/page.tsx
+++ 02-my-dashboard/app/dashboard/pokemon/[id]/page.tsx
@@ -1,14 +1,26 @@
+import { Pokemon } from "@/pokemons";
+
 interface Props {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
+
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: "force-cache", //TODO: Cambiar en futuro
+  }).then((res) => res.json());
+
+  console.log(`Se cargó: ${pokemon.name}`);
+  return pokemon;
+};
 
 export default async function PokemonPage({ params }: Props) {
   const { id } = await params;
-
-  console.log(id);
+  const pokemon = await getPokemon(id);
+  console.log(pokemon);
   return (
     <div>
       <h1>Pokemon {id}</h1>
+      {JSON.stringify(pokemon)}
     </div>
   );
 }
