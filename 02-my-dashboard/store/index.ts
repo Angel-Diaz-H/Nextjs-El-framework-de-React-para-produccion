5a7d16b73d4967e1a34b073216ab66e299146e83
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-07-03 07:33:31 -0600

    Exportando redux toolkit hooks

--- 02-my-dashboard/store/index.ts
+++ 02-my-dashboard/store/index.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counter/counterSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
   // Como hojas de nuestro store.
@@ -11,3 +12,6 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
