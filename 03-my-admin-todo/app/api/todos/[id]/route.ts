bdcff9064f35f3c1d42779af8fe7eaf2a8dc5a01
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-07-08 10:37:33 -0600

    Actualizar entradas - PUT

--- 03-my-admin-todo/app/api/todos/[id]/route.ts
+++ 03-my-admin-todo/app/api/todos/[id]/route.ts
@@ -23,3 +23,25 @@ export async function GET(request: Request, { params }: Segments) {
     todo,
   });
 }
+
+export async function PUT(request: Request, { params }: Segments) {
+  const { id } = await params;
+  const todo = await prisma.todo.findFirst({ where: { id } });
+
+  if (!todo) {
+    return NextResponse.json(
+      { message: "Todo no encontrado" },
+      { status: 404 },
+    );
+  }
+
+  const body = await request.json();
+  const updatedTodo = await prisma.todo.update({
+    where: { id },
+    data: { ...body },
+  });
+
+  return NextResponse.json({
+    updatedTodo,
+  });
+}
