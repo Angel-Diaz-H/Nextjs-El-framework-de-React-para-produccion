3299ce442e3122a19c1a2d1cdaef223f1e76f0c6
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-07-08 08:46:41 -0600

    Retornar una única entrada

---
+++ 03-my-admin-todo/app/api/todos/[id]/route.ts
@@ -0,0 +1,25 @@
+import prisma from "@/app/lib/prisma";
+import { NextResponse } from "next/server";
+
+interface Segments {
+  params: {
+    id: string;
+  };
+}
+
+export async function GET(request: Request, { params }: Segments) {
+  const { id } = params;
+
+  const todo = await prisma.todo.findFirst({ where: { id } });
+
+  if (!todo) {
+    return NextResponse.json(
+      { message: "Todo no encontrado" },
+      { status: 404 },
+    );
+  }
+
+  return NextResponse.json({
+    todo,
+  });
+}
