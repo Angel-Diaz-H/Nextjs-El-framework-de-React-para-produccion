c631ba972749cfa7374451e88d0956791ae261ce
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-07-14 12:43:33 -0600

    Listas las entradas

--- 03-my-admin-todo/app/dashboard/rest-todos/page.tsx
+++ 03-my-admin-todo/app/dashboard/rest-todos/page.tsx
@@ -1,3 +1,13 @@
-export default function RestTodosPage() {
-  return <></>;
+import prisma from "@/app/lib/prisma";
+
+export const metadata = {
+  title: "Listado de TODOS",
+  description: "Listado de TODOS",
+};
+
+export default async function RestTodosPage() {
+  const todos = await prisma.todo.findMany({ orderBy: { description: "asc" } });
+
+  console.log({ todos });
+  return <div>{JSON.stringify(todos)}</div>;
 }
