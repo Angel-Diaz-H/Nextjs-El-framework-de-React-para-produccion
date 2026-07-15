d46f7342d15c7fc1cce442753c37ed664b9d2f8e
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-07-15 15:49:03 -0600

    Preparar pantalla de Server Actions.

---
+++ 03-my-admin-todo/app/dashboard/server-todos/page.tsx
@@ -0,0 +1,22 @@
+import prisma from "@/app/lib/prisma";
+import { NewTodo, TodosGrid } from "@/todos";
+
+export const metadata = {
+  title: "Listado de TODOS",
+  description: "Listado de TODOS",
+};
+
+export default async function ServerTodosPage() {
+  const todos = await prisma.todo.findMany({ orderBy: { description: "asc" } });
+
+  return (
+    <>
+      <span className="mb-10 text-3xl font-bold">Server Actions</span>
+      <div className="mx-5 mb-5 w-full px-3">
+        <NewTodo />
+      </div>
+
+      <TodosGrid todos={todos} />
+    </>
+  );
+}
