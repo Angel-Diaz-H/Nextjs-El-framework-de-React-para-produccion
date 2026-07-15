d72863d6539cc1f2617d2b7d9b840fc8c8f935ee
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-07-14 18:08:39 -0600

    Actualizar server component

--- 03-my-admin-todo/todos/components/TodosGrid.tsx
+++ 03-my-admin-todo/todos/components/TodosGrid.tsx
@@ -1,17 +1,25 @@
 "use client";
 import { Todo } from "@/app/generated/prisma/client";
 import { TodoItem } from "./TodoItem";
-import * as api from "@/todos/helpers/todos";
+import * as todosApi from "@/todos/helpers/todos";
+import { useRouter } from "next/navigation";
 
 interface Props {
   todos: Todo[];
 }
 
 export const TodosGrid = ({ todos }: Props) => {
+  const router = useRouter();
+
+  const toggleTodo = async (id: string, complete: boolean) => {
+    const updatedTodo = await todosApi.updateTodo(id, complete);
+    router.refresh();
+  };
+
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} toggleTodo={api.updateTodo} />
+        <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
       ))}
     </div>
   );
