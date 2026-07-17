fae702a6c8917e1331f6a7d5789b8e63ba7add31
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-07-16 19:56:55 -0600

    Cookies - Server Side

--- 03-my-admin-todo/app/dashboard/cookies/page.tsx
+++ 03-my-admin-todo/app/dashboard/cookies/page.tsx
@@ -1,15 +1,22 @@
+import { cookies } from "next/headers";
 import { TabBar } from "../../../components/TabBar";
 export const metadata = {
   title: "Cookies Page",
   description: "Cookies Page",
 };
 
-export default function CookiesPage() {
+export default async function CookiesPage() {
+  const cookieStore = await cookies();
+  const cookieTab = cookieStore.get("selectedTab")?.value || "1"; // En algún momento son nulas.
+
+  const allCookies = cookieStore.getAll();
+
   return (
     <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-10">
         <span className="text-3xl">Tabs</span>
-        <TabBar />
+        <TabBar currentTab={+cookieTab} />
+        {JSON.stringify(allCookies)}
       </div>
     </div>
   );
