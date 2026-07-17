76f854c7ab3124a97dcbd6ce9bd7c1b7e095dcfe
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-07-17 17:29:45 -0600

    Total + impuesto

--- 03-my-admin-todo/app/dashboard/cart/page.tsx
+++ 03-my-admin-todo/app/dashboard/cart/page.tsx
@@ -1,3 +1,4 @@
+import { WidgetItem } from "@/components/WidgetItem";
 import { Product, products } from "@/products/data/products";
 import { ItemCard } from "@/shopping-cart";
 import { cookies } from "next/headers";
@@ -29,6 +30,11 @@ export default async function CartPage() {
   const cart = JSON.parse(cookiesStore.get("cart")?.value || "[]");
   const productsInCart = getProductInCart(cart);
 
+  const totalToPay = productsInCart.reduce(
+    (prev, current) => current.product.price * current.quantity + prev,
+    0,
+  );
+
   return (
     <div>
       <h1 className="text-5xl">Productos en el carrito</h1>
@@ -38,6 +44,19 @@ export default async function CartPage() {
           {productsInCart.map(({ product, quantity }) => (
             <ItemCard key={product.id} product={product} quantity={quantity} />
           ))}
+        </div>
+
+        <div className="flex w-full flex-col sm:w-4/12">
+          <WidgetItem title="Total a pagar.">
+            <div className="mt-2 flex justify-center gap-4">
+              <h3 className="text-3xl font-bold text-gray-700">
+                ${(totalToPay * 1.16).toFixed(2)}
+              </h3>
+            </div>
+            <span className="text-center font-bold text-gray-500">
+              {`IVA: $${(totalToPay * 0.16).toFixed(2)}`}
+            </span>
+          </WidgetItem>
         </div>
       </div>
     </div>
